74e21a965f7fb39df826ec6988897ad38524abc0
jim <gcobcqwe@yahoo.com.tw>
2021-06-28 12:28:53 +0800

    Implement routes, controllers and middlewares

---
+++ server/controllers/points.js
@@ -0,0 +1,116 @@
+/**
+ * BSD 2-Clause License
+ * Copyright (c) 2021, HITCON Agent Contributors
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+// TODO: Points controller
+const {ReasonPhrases, StatusCodes} = require('http-status-codes');
+const logger = require('../util/logger');
+const db = require('../models');
+const PointsService = require('../services/Points');
+const pointsServiceInstance = new PointsService(db);
+/**
+ * @description
+ * @param {Request} req
+ * @param {Response} res
+ */
+async function generateCode( req, res ) {
+  // TODO
+  // pointsServiceInstance.generateCode(uid, points)
+  try {
+    const uid = req.token.payload.sub;
+    const points = req.body.points;
+    const result = await pointsServiceInstance.generateCode(uid, points);
+    res.status(StatusCodes.OK).send(result);
+  } catch (e) {
+    logger.error(e);
+    res.status(StatusCodes.BAD_REQUEST).send({message: ReasonPhrases.BAD_REQUEST});
+  }
+}
+
+/**
+ * @description
+ * @param {Request} req
+ * @param {Response} res
+ */
+async function redeemCode( req, res ) {
+  // TODO
+  // pointsServiceInstance.redeemCode(uid, code)
+  try {
+    const uid = req.token.payload.sub;
+    const code = req.body.code;
+    const result = await pointsServiceInstance.redeemCode(uid, code);
+    res.status(StatusCodes.OK).send(result);
+  } catch (e) {
+    logger.error(e);
+    res.status(StatusCodes.BAD_REQUEST).send({message: ReasonPhrases.BAD_REQUEST});
+  }
+}
+
+/**
+ * @description
+ * @param {Request} req
+ * @param {Response} res
+ * @return {Promise<*>}
+ */
+async function transactions( req, res ) {
+  // TODO
+  // pointsServiceInstance.transactions(sender, receiver, points);
+  try {
+    const sender = req.token.payload.sub;
+    const receiver = req.body.receiver;
+    const points = req.body.points;
+    await pointsServiceInstance.transactions(sender, receiver, points);
+    res.status(StatusCodes.OK).send({message: ReasonPhrases.OK});
+  } catch (e) {
+    logger.error(e);
+    res.status(StatusCodes.BAD_REQUEST).send({message: ReasonPhrases.BAD_REQUEST});
+  }
+}
+
+/**
+ * @description
+ * @param {Request} req
+ * @param {Response} res
+ * @return {Promise<*>}
+ */
+async function transactionsHistory( req, res ) {
+  // TODO
+  // pointsServiceInstance.transactionsHistory(uid);
+  try {
+    const uid = req.token.payload.sub;
+    const result = await pointsServiceInstance.transactionsHistory(uid);
+    res.status(StatusCodes.OK).send(result);
+  } catch (e) {
+    logger.error(e);
+    res.status(StatusCodes.BAD_REQUEST).send({message: ReasonPhrases.BAD_REQUEST});
+  }
+}
+
+module.exports = {
+  generateCode,
+  redeemCode,
+  transactions,
+  transactionsHistory
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -25,35 +25,15 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-const config = {
-  server_port: process.env.SERVER_PORT || 4000,
-  cors_options: {
-    credentials: true,
-    origin: process.env.WEB_ENDPOINT,
-    optionsSuccessStatus: 200
-  },
-  server_auth_secret: process.env.SERVER_AUTH_SECRET || 'server_auth_secret',
-  cookie_secret: process.env.COOKIE_SECRET || 'cookie_secret',
-  cookie_max_age: 86400 * 1000 * 30 * 2,
-  [`database-${process.env.ENV}`]: {
-    migrationStorageTableName: 'sequelize_meta',
-    username: process.env.DATABASE_USERNAME || 'database_username',
-    password: process.env.DATABASE_PASSWORD || 'database_password',
-    database: process.env.DATABASE_NAME || 'postgres',
-    host: process.env.DATABASE_HOST || '127.0.0.1',
-    dialect: 'postgres',
-    logging: process.env.ENV === 'prod'? true: false,
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30 * 1000,
-      idle: 10 * 1000
-    },
-    define: {
-      createdAt: 'created_at',
-      updatedAt: 'updated_at'
-    }
-  }
-};
+const express = require('express');
+const users = require('../controllers/users');
+const router = express.Router();
+const {isAdmin} = require('../middlewares/auth');
 
-module.exports = config;
+router.get('/me', users.me);
+router.get('/', isAdmin, users.findAll);
+router.post('/', isAdmin, users.add);
+router.put('/', isAdmin, users.update);
+router.delete('/', isAdmin, users.destroy);
+
+module.exports = router;
