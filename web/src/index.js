1770c233795eef6f81879f6dee7f1c6d05e8e591
CanYu <fineaisa@gmail.com>
2021-10-02 15:20:00 +0800

    setup react environment

---
+++ web/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+
+const App = () => {
+  return (
+    <>
+      <header>HITCON 2021</header>
+    </>
+  )
+}
+
+ReactDom.render(<App />, document.getElementById("root"));
