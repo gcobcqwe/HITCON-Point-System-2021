b1f84da9b34d920a3e124af2896ba8d6ba019ff2
CanYu <fineaisa@gmail.com>
2021-10-03 02:39:50 +0800

    create some components with stlyed and prepare eslint environment

---
+++ web/src/components/Greeting.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin-bottom: 40px;
+`
+
+const Title = styled.div`
+  margin-bottom: 28px;
+`;
+
+const Description = styled.div`
+  line-height: 26px;
+`;
+
+const Greeting = ({title, description}) => {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+    </Container>
+  )
+}
+
+Greeting.defaultProps = {
+  title: "歡迎來到 HITCON 2120",
+  description: "7h1$ 1$ 4n 3x4mp£3 0ƒ £337 47 17$ ƒ1n3$7. 1 w1££ 74|{3 7h1$ 0pp0r7µn17¥ 70 r3m1nÐ ¥0µ 7h47 ¥0µ $h0µ£Ð 4£w4¥$ 937 ¥0µr |{1Ð$ p37 $p4¥3Ð 0r n3µ73r3Ð. N3v3r £34v3 h0m3 w17h0µ7 4 70w3£. 4nÐ n0 m4773r wh47 7h3¥ $4¥, 7h3r3 1$ n0 ([]\/\/ |_3\/3|_. |_0|_ h4x.", // Leet
+}
+
+export default Greeting;
---
+++ web/src/components/Schedule.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import Program from "./Program";
+
+const Wrapper = styled.div`
+  display:flex;
+  border-bottom: 3px solid #D6D6D6;
+  padding-bottom: 35px;
+  margin-bottom: 55px;
+  justify-content: space-between;
+
+  @media(max-width: 1040px) {
+    flex-direction: column;
+    padding-bottom: 25px;
+    margin-bottom: 15px;
+  }
+`
+
+const Title = styled.div`
+  font-size: 36px;
+`
+
+const Link = styled.a`
+  text-decoration: underline;
+  color: #CECECE;
+  font-size: 16px;
+  align-self: flex-end;
+  margin-left: 50px;
+`
+
+const Schedule = () => {
+  return (
+    <div>
+      <Wrapper>
+        <Title>進行中議程</Title>
+        <Link href="https://hitcon.org/2021/agenda/" target="_blank">查看完整議程表</Link>
+      </Wrapper>
+      <Program />
+      <Program />
+      <Program />
+    </div>
+  )
+}
+
+export default Schedule;
