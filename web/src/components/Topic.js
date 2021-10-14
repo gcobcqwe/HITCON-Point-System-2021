85b14528f02749ce24ae14eecd8d3f3746b33227
CanYu <fineaisa@gmail.com>
2021-10-14 23:40:33 +0800

    fix footer and topic

--- web/src/components/Topic.js
+++ web/src/components/Topic.js
@@ -3,71 +3,54 @@ import styled from "styled-components";
 import {Button} from "./Button";
 
 const Container = styled.div`
-  position: relative;
-  background: #777777 0% 0% no-repeat padding-box;
-  padding: 51px 35px 28px 43px;
 `;
 
-const Text = styled.div`
-  position: absolute;
-  top: -28px;
-  font-size: 36px;
-  font-weight: 900;
-`;
-
-const ContentWrapper = styled.div`
+const Image = styled.div`
   display: flex;
-
-  @media(max-width: 1040px) {
-    flex-direction: column-reverse;
-    align-items: center;
+  img {
+    width: 100%;
+    height: 100%;
+    border-top-left-radius: 20px;
+    border-top-right-radius: 20px;
   }
-}
+`;
+
+const Info = styled.div`
+  background: #002680;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  padding: 18px 35px 36px 30px;
 `;
 
 const Title = styled.div`
-  font-size: 28px;
+  font-size: 24px;
   margin-bottom: 22px;
 `;
 
 const Description = styled.div`
-  font-size: 14px;
+  font-size: 16px;
   line-height: 20px;
   max-width: 633px;
   margin-bottom: 27px;
 `;
 
-const Image = styled.img`
-  background: #D8D8D8 0% 0% no-repeat padding-box;
-  border-radius: 15px;
-  margin-left: 24px;
-
-  @media(max-width: 1040px) {
-    margin-left: 0px;
-    margin-bottom: 10px;
-  }
-`;
-
 const Topic = ({title, description, imageSrc, actionLink, actionText, actionToken}) => {
   return (
     <Container>
-      <Text>主題活動</Text>
-      <ContentWrapper>
-        <div>
-          <Title>{title}</Title>
-          <Description>{description}</Description>
-          <form method="POST" action={actionLink}>
-            { actionToken ?
-              <input type="hidden" name="token" value={actionToken} /> :
-              ""
-            }
-            <Button type ="submit">{actionText}</Button>
-          </form>
-        </div>
-        <div>
-          <Image src={imageSrc} />
-        </div>
-      </ContentWrapper>
+      <Image>
+        <img src={imageSrc} />
+      </Image>
+      <Info>
+        <Title>{title}</Title>
+        <Description>{description}</Description>
+        <form method="POST" action={actionLink}>
+          { actionToken ?
+            <input type="hidden" name="token" value={actionToken} /> :
+            ""
+          }
+          <Button type ="submit">{actionText}</Button>
+        </form>
+      </Info>
     </Container>
   )
 }
@@ -75,7 +58,7 @@ const Topic = ({title, description, imageSrc, actionLink, actionText, actionToke
 Topic.defaultProps = {
   title: "駭客狗狗",
   description: "喵～我是駭客狗狗，HITCON 最新開發的狗狗聊天機器人，不過我可沒有百寶袋唷！狗狗將會在 Telegram 以及 IRC 上協助大家參與 HITCON 2021，也許還會有一些小驚喜唷！喵～期待大會與各位相見！#聊天機器人 #駭客狗狗",
-  imageSrc: "https://via.placeholder.com/296x187",
+  imageSrc: "https://via.placeholder.com/361x190",
   actionLink: "https://t.me/hitcon_bot?start=00000001_6ce709f26bf3d745565024957ea1d003",
   actionText: "前往駭客狗狗",
   actionToken: false,
