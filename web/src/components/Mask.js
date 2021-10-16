9bb751c711bd1e8e0688f1ac137e64bbdce4140a
CanYu <fineaisa@gmail.com>
2021-10-17 01:50:35 +0800

    update User with RWD

---
+++ web/src/components/Mask.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Mask = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: black;
+  opacity: 0.55;
+  z-index: -1;
+`;
+
+export default Mask;
