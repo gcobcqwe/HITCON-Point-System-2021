05d23a4b445a421554b1d1a9a3b1713798945a52
Jim.Chien <gcobcqwe1@gmail.com>
2021-10-25 18:31:51 +0800

    Implement k6 loading test and race condition test.

---
+++ test/k6/users.js
@@ -0,0 +1,56 @@
+/**
+ * BSD 2-Clause License
+ * Copyright (c) 2021, HITCON Agent Contributors
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import http from 'k6/http';
+import { check } from 'k6';
+ 
+export let options = {
+  scenarios: {
+    constant_request_rate: {
+      executor: 'constant-arrival-rate',
+      rate: 100, // RPS, since timeUnit is the default 1s
+      duration: '30s',
+      preAllocatedVUs: 100, // how large the initial pool of VUs would be
+      maxVUs: 100, // if the preAllocatedVUs are not enough, we can initialize more; ulimit -n 4096
+    },
+  },
+};
+ 
+export default function () {
+  const host = '<HOST>';
+  const url = `${host}/api/v1/users/me`;
+  const accessToken = '<JWT>';
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`
+    },
+  };
+
+  let res = http.get(url, params);
+  check(res, { 'status was 200': (r) => r.status === 200});
+}
