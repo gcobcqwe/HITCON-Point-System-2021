c8d7da00930dde70ebb628c142005a34bb1f0359
CanYu <fineaisa@gmail.com>
2021-11-05 08:30:04 +0800

    1. add Redeem page for vendor 2. add API to exchange and redeem 3. add some zh lang for new component

--- web/src/components/User.js
+++ web/src/components/User.js
@@ -232,6 +232,22 @@ const UserContiner = () => {
     setIsTradningOpen(false);
   }
 
+  useEffect(() => {
+    const token = Cookies.get('token');
+    if (token === undefined) return;
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const payload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    const jsonPayload = JSON.parse(payload);
+    const { scope } = jsonPayload;
+    console.log(jsonPayload);
+    const result = scope.split(' ').pop();
+    setRole(result);
+    console.log("role: ", result);
+  },[]);
+
   return (
     <>
     <PositionFixed>
